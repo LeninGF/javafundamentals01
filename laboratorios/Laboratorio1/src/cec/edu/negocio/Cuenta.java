f5308e808e39a2b26751ea8d2a0397a250930149
Lenin G Falconí <enteatenea@gmail.com>
2020-03-09 21:02:40 -0500

    update

--- laboratorios/Laboratorio1/src/cec/edu/negocio/Cuenta.java
+++ laboratorios/Laboratorio1/src/cec/edu/negocio/Cuenta.java
@@ -8,11 +8,28 @@ public class Cuenta {
 	private float saldoCuenta;     // Es variable global
 	private String identificacionPropietario;
 	
+	// Creando el constructor (inicializa atributos):
+	public Cuenta(String numeroCuenta, float saldoCuenta, String identificacionPropietario){
+		this.numeroCuenta = numeroCuenta;
+		this.saldoCuenta = saldoCuenta;
+		this.identificacionPropietario = identificacionPropietario;
+	}
+
+	
+//	public Cuenta(String numeroCuenta, float saldoCuenta) {
+////		super();
+//		this.numeroCuenta = numeroCuenta;
+//		this.saldoCuenta = saldoCuenta;
+//	}
+
+
+
+
 	public String getNumeroCuenta() {
 		return numeroCuenta;
 	}
 	public void setNumeroCuenta(String numeroCuenta) {
-		this.numeroCuenta = numeroCuenta;
+		this.numeroCuenta = numeroCuenta;  // accedo al atributo de la clase con this
 	}
 	
 	public float getSaldoCuenta() {
