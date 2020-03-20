a0764547ab4005ea06655ef245e9981279e4cc10
LeninGF <enteatenea@gmail.com>
2020-03-19 21:48:12 -0500

    adding sub menu ahorros and sub menu corrientes

--- laboratorios/Laboratorio1/src/cec/edu/presentacion/Principal.java
+++ laboratorios/Laboratorio1/src/cec/edu/presentacion/Principal.java
@@ -1,21 +1,99 @@
 package cec.edu.presentacion;
 
-
 import java.util.Scanner;
 
 import cec.edu.negocio.Ahorros;
 import cec.edu.negocio.Corriente;
 import cec.edu.negocio.Servicio;
 
+/**
+ * Company: Atenatronix Corp
+ * 
+ * @author Lenin G. Falconi
+ * @version 1.0.0 {@Company} Atenatronix Corp
+ */
 public class Principal {
 	// La clase principal es el punto de ejecuci�n del proyecto
 	// El m�todo void main permite que se ejecute el proyecto como tal
-	
+
 	// ciclo desarrollo + compilacion + ejecucion
-	private static void menuPrincipal() {
-		Scanner scanner = new Scanner(System.in);  //inicializa un objeto Scanner para leer entradas teclado
-		int opcionIngresada=0;
-		
+	/**
+	 * @author leninml
+	 * 
+	 */
+	private void subMenuAhorros() {
+		/**
+		 * 
+		 */
+		int opcionIngresada = 0;
+		do {
+			System.out.println("Menu de Ahorros");
+			System.out.println("1.- Deposito");
+			System.out.println("2.- Retiro");
+			System.out.println("3.- Menu Principal");
+			System.out.println("\t\tSu opción actual es: " + opcionIngresada);
+			Scanner scanner2 = new Scanner(System.in);
+			opcionIngresada = scanner2.nextInt();
+			switch (opcionIngresada) {
+			case 1: {
+
+				break;
+			}
+			case 2:
+				break;
+			case 3:
+				System.out.println("Sub Menu Ahorros finalizado");
+				break;
+
+			default:
+				System.out.println("Opcion incorrecta");
+				System.out.println("Presiona enter para intentar de nuevo");
+				scanner2 = new Scanner(System.in);
+				scanner2.nextLine();
+			}
+		} while (opcionIngresada != 3);
+	}
+
+	private void subMenuCorriente() {
+		int opcionIngresada = 0;
+		do {
+			System.out.println("Sub Menu Cuentas Corrientes");
+			System.out.println("1.- Deposito");
+			System.out.println("2.- Retiro");
+			System.out.println("3.- Salir");
+			System.out.println("\t\tSu opción actual es: " + opcionIngresada);
+			Scanner scanner = new Scanner(System.in);
+			opcionIngresada = scanner.nextInt();
+
+			switch (opcionIngresada) {
+			case 1: {
+
+				break;
+			}
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+//				throw new IllegalArgumentException("Unexpected value: " + opcionIngresada);
+				System.out.println("Opcion incorrecta");
+				System.out.println("Presiona enter para intentar de nuevo");
+				scanner = new Scanner(System.in);
+				scanner.nextLine();
+
+			}
+
+		} while (opcionIngresada != 3);
+	}
+
+	private void menuPrincipal() {
+		/**
+		 * @author leninml
+		 * 
+		 */
+
+		int opcionIngresada = 0;
+
 		do {
 			System.out.println("====== Menu de opciones Principal======");
 			System.out.println("Selecciona un número de acuerdo a la transacción a realizar");
@@ -23,49 +101,55 @@ public class Principal {
 			System.out.println("2.- Cuentas Corriente");
 			System.out.println("3.- Pagos de Servicios");
 			System.out.println("4.- Salir");
-			System.out.println("Su opción actual es: "+opcionIngresada);	
+			System.out.println("\t\tSu opción actual es: " + opcionIngresada);
+			Scanner scanner = new Scanner(System.in); // inicializa un objeto Scanner para leer entradas teclado
 			opcionIngresada = scanner.nextInt();
-			
+
 			switch (opcionIngresada) {
-				case 1: {
-					
-					System.out.println("hola 1");
-					break;
-				}
-				case 2:{
-					System.out.println("hola 2");
-					break;
-				}
-				case 3:{
-					System.out.println("hola 3 ");
-					break;
-				}
-				case 4:{
-					System.out.println("Ha salido de la aplicación");
-					break;
-				}
-//				default:
+			case 1: {
+
+				this.subMenuAhorros();
+				break;
+			}
+			case 2: {
+				subMenuCorriente();
+				break;
+			}
+			case 3: {
+				System.out.println("hola 3 ");
+				break;
+			}
+			case 4: {
+				System.out.println("Ha salido de la aplicación");
+				break;
+			}
+			default:
 //					throw new IllegalArgumentException("Unexpected value: " + opcionIngresada);
-				}
-		}while(opcionIngresada!=4);
-		
+				System.out.println("******* Opcion Incorrecta *******");
+				System.out.println("Presionar enter para continuar");
+				scanner = new Scanner(System.in);
+				scanner.nextLine();
+				break;
+			}
+		} while (opcionIngresada != 4);
 
 	}
-	
-	public static void main(String []args) {
-		System.out.println("*******Laboratorio - 01 - *******");  // imprime a pantalla
-		// Crear una instancia (lowercamelcase) de la Clase Ahorros , la instancia ahorros es minuscula
+
+	public static void main(String[] args) {
+		System.out.println("*******Laboratorio - 01 - *******"); // imprime a pantalla
+		// Crear una instancia (lowercamelcase) de la Clase Ahorros , la instancia
+		// ahorros es minuscula
 //		Ahorros ahorros = new Ahorros("4102565478", (float) 50.20, "1719676239", "AHORRO FUTURO"); // NOTAR OPERACION DE CASTING EN FLOAT
 //		Corriente corriente = new Corriente("4102565478", (float)50.0, "1719676239", (float)2000.0);
 //		Servicio servicio = new Servicio("EEQ1719", (float) 20.0, "1719676239", false);
-		
+
 //		System.out.println("Numero de Cuenta: "+ahorros.getNumeroCuenta());
 //		System.out.println("Saldo actual: "+ahorros.getSaldoCuenta());
-		
-		menuPrincipal();
-		
-	}
-	
 
-	
+		Principal principal = new Principal();
+
+		principal.menuPrincipal();
+
+	}
+
 }
